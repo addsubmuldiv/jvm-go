92c787213b550b3a764120c9f34e8555471e584d
= <=>
2021-06-07 10:43:46 +0800

    init ch05

---
+++ ch05/rtda/thread.go
@@ -0,0 +1,32 @@
+package rtda
+
+type Thread struct {
+	pc    int    // 程序计数器
+	stack *Stack // jvm虚拟机栈
+}
+
+func NewThread() *Thread {
+	return &Thread{
+		stack: newStack(1024),
+	}
+}
+
+func (self *Thread) PC() int {
+	return self.pc
+}
+
+func (self *Thread) SetPC(pc int) {
+	self.pc = pc
+}
+
+func (self *Thread) PushFrame(frame *Frame) {
+	self.stack.push(frame)
+}
+
+func (self *Thread) PopFrame() *Frame {
+	return self.stack.pop()
+}
+
+func (self *Thread) CurrentFrame() *Frame {
+	return self.stack.top()
+}
